7d4f81acd7cabec501c8045fef53410775389e7d
BuildTools <santiago.moreno.2009@gmail.com>
2021-12-24 19:32:09 -0300

    feat: added updateUser function to user service

--- services/user-service.js
+++ services/user-service.js
@@ -1,20 +1,19 @@
 const { User } = require("../models");
 const repository = require("../repositories/base-repository.js");
 
-//Attributes to use in repository
-const attributes = (req) => {
-  const { body } = req;
-  const { email } = body;
-  return {
-    id: req.params.id,
-    toFind: { email },
-    toCreateOrUpdate: body,
-  };
-};
-
 module.exports = {
   createUser: async (req, res) => {
-    const createdUser = await repository.create(User, attributes(req));
+    //Attributes for repository
+    let { email } = req.body;
+    const attributes = {
+      toFind: { email },
+      toCreate: req.body,
+    };
+
+    //Repository
+    const createdUser = await repository.create(User, attributes);
+
+    //Responses
     if (createdUser === false) {
       const errorToThrow = new Error();
       errorToThrow.status = 403;
@@ -24,5 +23,28 @@ module.exports = {
 
     //TODO: Encrypt password and generate token
     return createdUser;
+  },
+
+  updateUser: async (req, res) => {
+    //Attributes for repository
+    const { id } = req.params;
+    const attributes = {
+      toFind: { id },
+      toUpdate: req.body,
+    };
+
+    //Repository
+    const updatedUser = await repository.update(User, attributes);
+
+    //Responses
+    if (updatedUser === false) {
+      const errorToThrow = new Error();
+      errorToThrow.status = 404;
+      errorToThrow.message = "User not found";
+      throw errorToThrow;
+    }
+
+    //Todo: add encryption in case of password
+    return updatedUser;
   },
 };
