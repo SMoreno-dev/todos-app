e3db3ff0ab1a8d584c302a8d882783f40e62d2e9
BuildTools <santiago.moreno.2009@gmail.com>
2021-12-24 18:46:50 -0300

    feat: implemented service in user controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,7 +1,12 @@
 const catchAsync = require("../utils/catchAsync");
+const userService = require("../services/user-service.js");
 
 module.exports = {
   create: catchAsync(async (req, res, next) => {
-    return res.status(200).json("TODO: Add user registration");
+    const createdUser = await userService.createUser(req, res);
+    res.status(200).json({
+      message: "Successfully created new user",
+      body: createdUser,
+    });
   }),
 };
