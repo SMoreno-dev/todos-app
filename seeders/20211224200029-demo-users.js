34e6a025f98e9c2848733b2609e9e0788cf96f57
BuildTools <santiago.moreno.2009@gmail.com>
2021-12-24 20:21:57 -0300

    feat: added password encryption in demo users seed file

--- seeders/20211224200029-demo-users.js
+++ seeders/20211224200029-demo-users.js
@@ -1,22 +1,28 @@
 "use strict";
-const dotenv = require("dotenv");
-dotenv.config();
 
-//Mock data
-const mockUsers = [];
-for (let i = 1; i < 11; i++) {
-  let user = {
-    email: `user${i}@gmail.com`,
-    password: `${process.env.MOCK_USER_PASSWORD + i}`,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  };
-  mockUsers.push(user);
-}
+require("dotenv").config();
+const encryptPassword = require("../utils/encryptPassword");
+
+const createUsers = async () => {
+  let arr = [];
+  for (let i = 1; i < 11; i++) {
+    let password = await encryptPassword(process.env.MOCK_USER_PASSWORD + i);
+    if (password !== undefined) {
+      arr.push({
+        email: `user${i}@gmail.com`,
+        password,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      });
+    }
+  }
+  return arr;
+};
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert("Users", mockUsers);
+    const arrayOfUsers = await createUsers();
+    await queryInterface.bulkInsert("Users", arrayOfUsers);
   },
 
   down: async (queryInterface, Sequelize) => {
