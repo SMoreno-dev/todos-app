4c5486cdea04a9999e63ce889518d28314a2d527
BuildTools <santiago.moreno.2009@gmail.com>
2021-12-24 19:31:39 -0300

    feat: added update function to base repository; refactor: made function names less model-specific

--- repositories/base-repository.js
+++ repositories/base-repository.js
@@ -2,14 +2,29 @@ const { sequelize } = require("../models");
 
 module.exports = {
   create: async (model, attributes) => {
-    const createdUser = await sequelize.transaction(async (t) => {
+    const createdRow = await sequelize.transaction(async (t) => {
       const [created, success] = await model.findOrCreate({
         where: attributes.toFind,
-        defaults: attributes.toCreateOrUpdate,
+        defaults: attributes.toCreate,
       });
 
       return success ? created : false;
     });
-    return createdUser;
+    return createdRow;
+  },
+
+  update: async (model, attributes) => {
+    const findRow = await model.findOne({ where: attributes.toFind });
+    if (!findRow) return false;
+
+    const updatedRow = await sequelize.transaction(async (t) => {
+      await model.update(attributes.toUpdate, {
+        where: attributes.toFind,
+      });
+
+      return findRow;
+    });
+
+    return updatedRow;
   },
 };
