86ed28fdf6ef175bc2d64eee401cdce08f3d04ec
BuildTools <santiago.moreno.2009@gmail.com>
2021-12-24 22:44:16 -0300

    fix: fixed errors not logging in error handler in server.js

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 app.listen(PORT, HOST, () => {
   console.log("Server running on port ", PORT);
 });
@@ -36,6 +37,7 @@ app.use((req, res, next) => {
 
 //Error handler
 app.use((err, req, res, next) => {
+  console.error(err);
   const status = err.status || 500;
   res.status(status).json({
     status: status,
