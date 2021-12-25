6f78e1e683fb9f36c14229d6ddf51fd6cae44b44
BuildTools <santiago.moreno.2009@gmail.com>
2021-12-25 20:14:56 -0300

    feat: adding environment variables for testing, setting up sequelize config; fix: fixed bad dialect option in sequelize config

--- config/config.js
+++ config/config.js
@@ -1,7 +1,16 @@
 const { config } = require("dotenv");
 config();
 
-const { DB_USER, DB_PASSWORD, DB_NAME, DB_HOST } = process.env;
+const {
+  DB_USER,
+  DB_PASSWORD,
+  DB_NAME,
+  DB_HOST,
+  TEST_DB_USER,
+  TEST_DB_PASSWORD,
+  TEST_DB_NAME,
+  TEST_DB_HOST,
+} = process.env;
 
 module.exports = {
   development: {
@@ -9,6 +18,13 @@ module.exports = {
     password: DB_PASSWORD,
     database: DB_NAME,
     host: DB_HOST,
-    dialect: "postgresql",
+    dialect: "postgres",
+  },
+  test: {
+    username: TEST_DB_USER,
+    password: TEST_DB_PASSWORD,
+    database: TEST_DB_NAME,
+    host: TEST_DB_HOST,
+    dialect: "postgres",
   },
 };
