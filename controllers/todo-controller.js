f9a06516f6ff41b7025c4ca1c95d7ecf36da613b
BuildTools <santiago.moreno.2009@gmail.com>
2021-12-25 18:25:07 -0300

    feat: added todo controller

---
+++ controllers/todo-controller.js
@@ -0,0 +1,35 @@
+const catchAsync = require("../utils/catchAsync");
+const todoService = require("../services/todo-service");
+
+module.exports = {
+  create: catchAsync(async (req, res, next) => {
+    const createdTodo = await todoService.createTodo(req, res);
+    res.status(201).json({
+      message: "Successfully created TODO",
+      body: createdTodo,
+    });
+  }),
+
+  list: catchAsync(async (req, res, next) => {
+    const listTodos = await todoService.listTodos(req, res);
+    res.status(200).json({
+      message: "Returning TODOs",
+      body: listTodos,
+    });
+  }),
+
+  update: catchAsync(async (req, res, next) => {
+    const updatedTodo = await todoService.updateTodo(req, res);
+    res.status(200).json({
+      message: "Successfully updated TODO",
+      body: updatedTodo,
+    });
+  }),
+
+  delete: catchAsync(async (req, res, next) => {
+    const deletedTodo = await todoService.deleteTodo(req, res);
+    res.status(200).json({
+      message: "Successfully deleted TODO",
+    });
+  }),
+};
