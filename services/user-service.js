e245b9237a7ccbe8baca708665a31610b352fc76
BuildTools <santiago.moreno.2009@gmail.com>
2021-12-24 22:04:47 -0300

    refactor: moved transaction functions from repository to user service

--- services/user-service.js
+++ services/user-service.js
@@ -1,5 +1,8 @@
-const { User } = require("../models");
+const { User, sequelize } = require("../models");
 const repository = require("../repositories/base-repository.js");
+const buildUserObject = require("../utils/buildUserObject");
+const encryptPassword = require("../utils/encryptPassword");
+const { generateAccessToken } = require("../utils/jsonwebtoken");
 
 module.exports = {
   createUser: async (req, res) => {
@@ -11,18 +14,33 @@ module.exports = {
     };
 
     //Repository
-    const createdUser = await repository.create(User, attributes);
+    return await sequelize.transaction(async (t) => {
+      const createdUser = await repository.create(User, attributes, t);
 
-    //Responses
-    if (createdUser === false) {
+      //Responses
+      if (createdUser === false) {
+        const errorToThrow = new Error();
+        errorToThrow.status = 403;
+        errorToThrow.message = "This user email already exists";
+        throw errorToThrow;
+      }
+
+      //Adds auth token to response
+      const token = generateAccessToken(createdUser.dataValues);
+      if (token !== null) {
+        const userWithToken = Object.assign(buildUserObject(createdUser), {
+          token,
+        });
+        console.log(userWithToken);
+        return userWithToken;
+      }
+
       const errorToThrow = new Error();
-      errorToThrow.status = 403;
-      errorToThrow.message = "This user email already exists";
+      errorToThrow.status = 500;
+      errorToThrow.message = "Internal Server error";
+      console.log("Something went wrong in user-service.js");
       throw errorToThrow;
-    }
-
-    //TODO: Encrypt password and generate token
-    return buildUserObject(createdUser);
+    });
   },
 
   updateUser: async (req, res) => {
@@ -33,18 +51,26 @@ module.exports = {
       toUpdate: req.body,
     };
 
-    //Repository
-    const updatedUser = await repository.update(User, attributes);
+    const { password } = attributes.toUpdate;
 
-    //Responses
-    if (updatedUser === false) {
-      const errorToThrow = new Error();
-      errorToThrow.status = 404;
-      errorToThrow.message = "User not found";
-      throw errorToThrow;
+    if (password) {
+      attributes.toUpdate.password = encryptPassword(req.password);
     }
 
-    //Todo: add encryption in case of password
+    //Repository
+    await sequelize.transaction(async (t) => {
+      const updateUser = await repository.update(User, attributes, t);
+
+      //Responses
+      if (updateUser === false) {
+        const errorToThrow = new Error();
+        errorToThrow.status = 404;
+        errorToThrow.message = "User not found";
+        throw errorToThrow;
+      }
+    });
+
+    const updatedUser = await repository.getById(User, attributes);
     return buildUserObject(updatedUser);
   },
 };
