232c3bf3618f5e72761996ca683b374015a75f78
BuildTools <santiago.moreno.2009@gmail.com>
2021-12-25 19:53:25 -0300

    feat: created token authentication middleware

---
+++ middleware/authentication.js
@@ -0,0 +1,20 @@
+const { validateToken } = require("../utils/jsonwebtoken");
+const FORBIDDEN_CODE = require("../constants/http-status").FORBIDDEN;
+const FORBIDDEN_MESSAGE = require("../constants/default-constants").FORBIDDEN;
+
+module.exports = (req, res, next) => {
+  // Sends the request to validate the token
+  const payload = validateToken(req);
+
+  // The payload being null means of the follow:
+  //   - the token didn't exist in the request
+  //   - the token wasn't valid
+  //   - the token expired
+  if (payload === null) return throwError(FORBIDDEN_CODE, FORBIDDEN_MESSAGE);
+
+  // Adds the payload of the token to the request. This will let access
+  // it from another place without revalidating the token
+  req.tokenPayload = payload;
+
+  next();
+};
