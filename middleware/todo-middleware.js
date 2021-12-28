cb7b1800a9367b23b23c10a653863dfef942f803
SMoreno-dev <santiago.moreno.2009@gmail.com>
2021-12-28 03:14:12 -0300

    fix: fixed todo middleware not recognizing boolean properties in requests

--- middleware/todo-middleware.js
+++ middleware/todo-middleware.js
@@ -1,9 +1,11 @@
 const code = require("../constants/http-status");
 const message = require("../constants/todo-constants");
 
+const isUndefined = (prop) => typeof prop === undefined;
+
 const validateCreateTodo = (req, res, next) => {
-  let { title, content, userId } = req.body;
-  if (![title, content, userId].every(Boolean)) {
+  let { title, content, completed, userId } = req.body;
+  if ([title, content, completed, userId].some(isUndefined)) {
     return res
       .status(code.BAD_REQUEST)
       .json({ message: message.BAD_CREATE_TODO_REQUEST });
@@ -13,8 +15,8 @@ const validateCreateTodo = (req, res, next) => {
 };
 
 const validateUpdateTodo = (req, res, next) => {
-  let { title, content, userId } = req.body;
-  if (![title, content, userId].some(Boolean)) {
+  let { title, content, completed } = req.body;
+  if ([title, content, completed].every(isUndefined)) {
     return res
       .status(code.BAD_REQUEST)
       .json({ message: message.BAD_UPDATE_TODO_REQUEST });
